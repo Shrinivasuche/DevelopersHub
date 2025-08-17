758b282323bd99bd1b811d8aea76eef0a1cbc34e
Shrinivasuche <shrinivasuche@gmail.com>
2025-08-17 10:53:36 +0530

    resolved the cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://developers-hub-blond.vercel.app/"
+        "https://developers-hub-blond.vercel.app"
     ],
     credentials: true
 }));
